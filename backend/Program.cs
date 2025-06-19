b26e33668cf2b4d62880ae023d47de0751aa8004
44Matt44 <matt123.3a@gmail.com>
2025-06-19 12:37:19 +0200

    Update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -8,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "VideoHub API", Version = "v1" });
+});
 
 var app = builder.Build();
 
