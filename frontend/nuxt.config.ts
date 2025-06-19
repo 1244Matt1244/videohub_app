1df2a0a0c5db667f9d83b3f1263628c2ad5f58de
44Matt44 <matt123.3a@gmail.com>
2025-06-19 22:27:41 +0200

    Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,5 +6,11 @@ export default defineNuxtConfig({
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5000'
     }
+  },
+  app: {
+    head: {
+      title: 'VideoHub',
+      meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }]
+    }
   }
 })
