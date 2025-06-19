b465295a448ed39f3e7bf5582196ffd5f2d81b3d
44Matt44 <matt123.3a@gmail.com>
2025-06-19 19:09:14 +0200

    Update VideoCard.vue

---
+++ frontend/components/VideoCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card shadow-md bg-base-100">
+    <figure><img :src="video.thumbnail" alt="Video thumbnail" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">{{ video.title }}</h2>
+      <p>{{ video.description }}</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-accent" @click="watch">Watch</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ video: { id: string, title: string, thumbnail: string, description: string } }>()
+
+function watch() {
+  // redirect to video player
+}
+</script>
