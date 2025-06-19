0e7bd47828247ce72fb29f89c0399f1517a5ec62
44Matt44 <matt123.3a@gmail.com>
2025-06-19 19:04:09 +0200

    Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,10 @@
 import { defineNuxtConfig } from 'nuxt'
+
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
-  runtimeConfig: { public: { apiBase: process.env.NUXT_PUBLIC_API_BASE } }
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5000'
+    }
+  }
 })
