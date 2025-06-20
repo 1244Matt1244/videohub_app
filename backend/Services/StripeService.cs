bf164b04b01476400d935c63679b5dcc14221961
44Matt44 <matt123.3a@gmail.com>
2025-06-20 21:42:49 +0200

    Update StripeService.cs

--- backend/Services/StripeService.cs
+++ backend/Services/StripeService.cs
@@ -1,32 +1,30 @@
 using Microsoft.Extensions.Configuration;
 using Stripe;
-using System;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace backend.Services
 {
     public class StripeService
     {
+        private readonly IConfiguration _config;
+
         public StripeService(IConfiguration config)
         {
-            var secretKey = config["Stripe:SecretKey"];
-            if (string.IsNullOrWhiteSpace(secretKey))
-                throw new ArgumentNullException("Stripe secret key missing in config");
-
-            StripeConfiguration.ApiKey = secretKey;
+            _config = config;
+            StripeConfiguration.ApiKey = _config["Stripe:SecretKey"];
         }
 
-        public async Task<PaymentIntent> CreatePaymentIntent(long amount, string currency = "usd")
+        public PaymentIntent CreatePaymentIntent(long amount, string currency)
         {
             var options = new PaymentIntentCreateOptions
             {
                 Amount = amount,
                 Currency = currency,
-                PaymentMethodTypes = new[] { "card" }
+                PaymentMethodTypes = new List<string> { "card" }
             };
 
             var service = new PaymentIntentService();
-            return await service.CreateAsync(options);
+            return service.Create(options);
         }
     }
 }
