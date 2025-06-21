e33a37407ae1ca3e28eccb7b0801c0af4f0554cd
44Matt44 <matt123.3a@gmail.com>
2025-06-22 00:24:32 +0200

    Delete Interfaces/IVideoService.cs

--- Interfaces/IVideoService.cs
+++
@@ -1,9 +0,0 @@
-namespace videohub_app.backend.Interfaces
-{
-    public interface IVideoService
-    {
-        Task<Video> UploadVideo(VideoUploadRequest request);
-        Task DeleteVideo(string videoId);
-        Task<Video> GetVideoDetails(string videoId);
-    }
-}
