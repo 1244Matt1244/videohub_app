3026158735748ec37d6770d898c623f4feff4830
44Matt44 <matt123.3a@gmail.com>
2025-06-22 00:52:44 +0200

    Create MuxAsset.cs

---
+++ backend/Models/MuxAsset.cs
@@ -0,0 +1,9 @@
+namespace videohub_app.backend.Models
+{
+    public class MuxAsset
+    {
+        public string Id { get; set; }
+        public string PlaybackId { get; set; }
+        public string Status { get; set; }
+    }
+}
